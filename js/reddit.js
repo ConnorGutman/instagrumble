f335ddd9ef03a8633d05c4f26281ad1129865aa1
Connor Gutman <connorgutman@gmail.com>
2016-01-06 18:50:40 -0700

    removed Ads

--- js/reddit.js
+++ js/reddit.js
@@ -14,41 +14,9 @@ $.getJSON(redditAPI, function(json) {
   var postDate = [];
   var postDateHuman = [];
   var source = [];
-  var adCount = 0;
 
   //html for a card
   var card = "<div class='col-sm-8 col-sm-offset-2'><div class='card'> <div class='card-header'> <p class='card-top'> <span> <img src='img/pug.png' alt='...' class='avatar'> </span> <span> <a class='author permalink'></a> </span> <span class='time pull-right'></span> </p> </div> <img class='card-img-top pug-photo' src='' alt=''> <div class='card-block'> <i class='like fa fa-heart-o'></i> <i class='fa fa-comment-o'></i> <i class='fa fa-share-square-o'></i> <i class='fa fa-ellipsis-v pull-right'></i> <hr> <p> <strong class='ups'></strong> likes</p> <p class='card-text'> <span> <a class='author'></a> </span> <span class='title'></span> <span class='hashtag'></span> </p> </div></div></div>";
-
-
-  var adcard = "    <div class='col-sm-8 col-sm-offset-2'> \
-        <div class='card'> \
-          <div class='card-header'> \
-            <p class='card-top'> \
-              <span> \
-                <img src='img/pug.png' alt='...' class='avatar'> </span> \
-              <span> <a class='author permalink'>Google</a> </span> \
-              <span class='time pull-right'> </span> \
-            </p> \
-          </div> \
-          <div class='card-block' style='width:100%; height:40%;''> \
-          <script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script> \
-  <!-- instagrumble --> \
-  <ins class='adsbygoogle' style='display:block' data-ad-client='ca-pub-4676498492886978' data-ad-slot='2014569934' data-ad-format='auto'></ins> \
-  <script> \
-  (adsbygoogle = window.adsbygoogle || []).push({}); \
-  </script> \
-    </div> \
-          <div class='card-block'> <i class='like fa fa-heart-o'></i> <i class='fa fa-comment-o'></i> <i class='fa fa-share-square-o'></i> <i class='fa fa-ellipsis-v pull-right'></i> \
-            <hr> \
-            <p> \
-            <p class='card-text'> \
-              <span> <a class='author'>Google</a> </span> \
-              <span class='title'>Sponsored Ad</span> \
-              <span class='hashtag'></span> \
-            </p> \
-          </div> \
-        </div> \
-      </div>";
 
   //Loop through and assign each variable
   for (i = 0; i < postCount; i++) { //Note that the first two posts are stickied
@@ -83,12 +51,6 @@ $.getJSON(redditAPI, function(json) {
     postDateHuman[i] = moment(postDate[i]).fromNow();
     console.log('Post Date: ', postDateHuman[i]);
     $(pug + " .time").text(postDateHuman[i]);
-
-    //Every 10 posts insert an ad
-    adCount++;
-    if (adCount == 1 || adCount == 15 || adCount == 30) {
-      $("#home").append("<div class='row' id='pugad" + "\'>" + adcard + "</div>");
-    }
 
     //Hide Videos, Albums, Instagram posts, and Selfposts
     if (photo[i].match("^http://imgur.com/") || photo[i].match("^https://imgur.com/")) {
